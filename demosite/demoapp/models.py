3b64e05f6e9967dafd6a55b08f1705642a94ac01
Michael Dehaney <dehaney21@gmail.com>
2022-05-19 14:53:41 -0500

    Update models.py

--- demosite/demoapp/models.py
+++ demosite/demoapp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    # def __str__(self):
-    #     return self.name
+     def __str__(self):
+        return self.name
 
     name = models.CharField(max_length=100)
     author = models.CharField(default='Tim Brown', max_length=100)
